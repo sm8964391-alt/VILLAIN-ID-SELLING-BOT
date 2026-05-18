e0d3e691d427a559f6fc359c3cc4bc0c6cbfe9fa
sm8964391-alt <sm8964391@gmail.com>
2026-05-18 09:17:13 +0530

    Update admin IDs and credentials in bot.py

--- bot.py
+++ bot.py
@@ -29,18 +29,18 @@ from telethon.errors import FloodWaitError, SessionPasswordNeededError
 
 # ─── CONFIG ──────────────────────────────────────────────────────────────────
 BOT_TOKEN      = "8663089754:AAGhkc-WhuKhu0mYblOced77OkaUrfcq3QE"
-ADMIN_IDS      = [5286579067]
+ADMIN_IDS      = [5286579067,6740955542]
 ADMIN_GROUP_ID = -1003837647645
 LOG_CHANNEL_ID = -1003916799207
 API_ID         = 30191201
 API_HASH       = "5c87a8808e935cc3d97958d0bb24ff1f"
-UPI_ID         = "ashishchoudhary21@fam"
+UPI_ID         = "rohan242009@fam"
 DB_PATH        = "numberstore001982.db"
 IST            = timezone(timedelta(hours=5, minutes=30))
 
 # Gmail credentials for auto-approval
 GMAIL_USER     = "ashishchoudharyrj21@gmail.com"
-GMAIL_APP_PASS = "wtqa tpnp sart sjyd"
+GMAIL_APP_PASS = "nsbh dkqi jqil wwuw"
 FAMAPP_EMAILS  = ["no-reply@famapp.in"]
 
 OXAPAY_MERCHANT_KEY = "R7GWJN-NPCMVX-H3QYHQ-FL2DJA"
